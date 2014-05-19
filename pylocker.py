f97f31a59e28b247fec96f46e95aa6a3593c02ee
Nathan Osman <admin@quickmediasolutions.com>
2014-05-18 17:35:28 -0700

    Cleaned up the code a bit and made it a bit more robust.

--- pylocker.py
+++ pylocker.py
@@ -1,21 +1,25 @@
-#!/usr/bin/env python3
-import sys
+#!/usr/bin/env python
+import argparse
 import dbus
+import sys
 
-bus = dbus.SessionBus()
-proxy_object = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
-screensaver = dbus.Interface(proxy_object,
-              dbus_interface='org.gnome.ScreenSaver')
-
-def screenlocker(a):
-    if a == 'lock':
+def run_command(args):
+    """Runs the specified command."""
+    bus = dbus.SessionBus()
+    proxy_object = bus.get_object('org.gnome.ScreenSaver', '/org/gnome/ScreenSaver')
+    screensaver = dbus.Interface(proxy_object, dbus_interface='org.gnome.ScreenSaver')
+    if args.command == 'lock':
+        print 'Locking the screen...'
         screensaver.Lock()
-    elif a == 'unlock':
-        dbusfalse = dbus.Boolean(0)
-        screensaver.SetActive(dbusfalse)
     else:
-        print 'Use "lock" or "unlock" to lock or unlock the screen'
+        print 'Unlocking the screen...'
+        screensaver.SetActive(dbus.Boolean(0))
 
-if __name__ == "__main__":
-    import sys
-    screenlocker(sys.argv[1])
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Controls the lock screen')
+    parser.add_argument('command',
+                        metavar='COMMAND',
+                        type=str,
+                        choices=['lock', 'unlock'],
+                        help='either "lock" or "unlock"')
+    run_command(parser.parse_args())
